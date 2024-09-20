77b8b834952848a48b0917914dcb07b6667c98ec
tharushamadura <janajanaka555@gmail.com>
2024-09-20 19:59:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " ",
+SESSION_ID: process.env.SESSION_ID || " 5ERFmCaa#INTSdKyysWqtvDgQ9G-bcDe9dcktD7oXe7Hr60kM7IA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
